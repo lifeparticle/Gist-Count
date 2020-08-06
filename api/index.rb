b1b3361935b857906034fcfce666a112b422cd40
lifeparticle <mahbubzaman17@gmail.com>
2020-08-06 12:38:56 +1000

    init

---
+++ api/index.rb
@@ -0,0 +1,23 @@
+require 'victor'
+
+Handler = Proc.new do |req, res|
+	svg = Victor::SVG.new width: 140, height: 100, style: { background: '#ddd' }
+
+	svg.build do
+		rect x: 10, y: 10, width: 120, height: 80, rx: 10, fill: '#666'
+
+		circle cx: 50, cy: 50, r: 30, fill: 'yellow'
+		circle cx: 58, cy: 32, r: 4, fill: 'black'
+		polygon points: %w[45,50 80,30 80,70], fill: '#666'
+
+		3.times do |i|
+			x = 80 + i*18
+			circle cx: x, cy: 50, r: 4, fill: 'yellow'
+		end
+	end
+
+	#svg.save 'pacman'
+	res.status = 200
+	res['Content-Type'] = 'image/svg+xml; charset=utf-8'
+	res.body = svg
+end
