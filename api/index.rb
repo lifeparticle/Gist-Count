e425536d82014f959b1866ec195a2d84d2f29ddb
Mahbub Zaman <mahbubzaman17@gmail.com>
2024-06-14 22:10:55 +1000

    Update index.rb

--- api/index.rb
+++ api/index.rb
@@ -7,7 +7,7 @@ Handler = Proc.new do |req, res|
   if req.query.has_key?("username")
     username = req.query["username"]
     gist_count = fetch_gist_count(username)
-    message = "#{username}'s gist count is: #{gist_count}"
+    message = "Gist count: #{gist_count}"
     status = 200
   else
     message = "Username not found"
@@ -19,9 +19,10 @@ Handler = Proc.new do |req, res|
   colors = get_colors(theme)
 
   svg = Victor::SVG.new viewBox: "0 0 #{width} 20", height: '20'
+  add_gradient(svg) if theme == "light"
   svg.rect x: 0, y: 0, width: width, height: 20, rx: 5, ry: 5, fill: colors[:background]
   svg.build do
-    g font_size: 10, font_family: 'arial', fill: colors[:text], text_shadow: colors[:shadow] do
+    g font_size: 10, font_family: 'Verdana, Arial, sans-serif', fill: colors[:text], text_shadow: colors[:shadow] do
       text message, x: width / 2, y: 13.5, text_anchor: 'middle'
     end
   end
@@ -77,34 +78,4 @@ def add_gradient(svg)
       end
     end
   end
-end
-
-Handler = Proc.new do |req, res|
-  if req.query.has_key?("username")
-    username = req.query["username"]
-    gist_count = fetch_gist_count(username)
-    message = "#{username}'s gist count is: #{gist_count}"
-    status = 200
-  else
-    message = "Username not found"
-    status = 404
-  end
-
-  theme = req.query["theme"] || "dark"
-  width = calculate_width(message)
-  colors = get_colors(theme)
-
-  svg = Victor::SVG.new viewBox: "0 0 #{width} 20", height: '20'
-  add_gradient(svg) if theme == "light"
-  svg.rect x: 0, y: 0, width: width, height: 20, rx: 5, ry: 5, fill: colors[:background]
-  svg.build do
-    g font_size: 10, font_family: 'arial', fill: colors[:text], text_shadow: colors[:shadow] do
-      text message, x: width / 2, y: 13.5, text_anchor: 'middle'
-    end
-  end
-
-  res.status = status
-  res['Cache-Control'] = "public, max-age=#{86_400}"
-  res['Content-Type'] = 'image/svg+xml'
-  res.body = svg.render
 end
